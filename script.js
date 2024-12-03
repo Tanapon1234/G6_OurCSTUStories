bdd7cd0244e4820f22d7377ba1134ca104e63972
dr-benny <t.tanapon.b@gmail.com>
2024-12-03 03:09:38 -1000

    add all comment section

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
       const firstname = document.getElementById("form_name").value;
       const lastname = document.getElementById("form_lastname").value;
       const email = document.getElementById("form_email").value;
+      const comment = document.getElementById("form_message").value;
 
+      const commentsContainer = document.getElementById("commentsContainer");
+      const commentsSection = document.getElementById("commentsSection");
       const popupMessage = `
         <strong>Name:</strong> ${firstname} ${lastname}
         <br><strong>Email:</strong> ${email}
@@ -18,7 +21,21 @@ document.addEventListener("DOMContentLoaded", function () {
       const modal = new bootstrap.Modal(
         document.getElementById("thankYouModal")
       );
+      const commentCard = document.createElement("div");
+      commentCard.classList.add("card", "mb-3");
+      commentCard.innerHTML = `
+            <div class="card-body  text-start">
+              <h6 class="card-title"><strong>Name:</strong> ${firstname} ${lastname}</h6>
+             <div><strong>Comment:</strong> ${comment}</div>
+             </div>
+        `;
+
+      commentsContainer.prepend(commentCard);
       modal.show();
+
+      if (commentsContainer.children.length > 0) {
+        commentsSection.style.display = "block";
+      }
 
       document.getElementById("contact-form").reset();
     });
